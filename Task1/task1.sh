94aacc525ad8e0d621c2f03643041c5a75a371f5
trikaynalamada <nalamada.trikay@gmail.com>
2018-05-23 20:46:11 +0530

    first commit

---
+++ Task1/task1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir Coding101
+cd Coding101
+mkdir Task1
+cd ..
+mv task1.sh Coding101/Task1/task1.sh
+
---
+++ Task2/task2.sh
@@ -0,0 +1,5 @@
+cd Coding101
+mkdir Task2
+cd Task2
+wget -O hello.txt https://github.com/php/playground/blob/master/HelloWorld.txt
+grep 'World' hello.txt > output.txt
